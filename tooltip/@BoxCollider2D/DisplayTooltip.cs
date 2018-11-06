91f6298c10fe99ea1e48faec674e701287631b16
JaeJunLee <markman0510@gmail.com>
2018-11-06 13:46:50 +0900

    [Tooltip]Box collider2D에 붙이기
    기존 UITooltip 상속 후 변경

---
+++ tooltip/@BoxCollider2D/DisplayTooltip.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DisplayTooltip : MonoBehaviour
+{
+    public string key = "";
+
+    void OnTooltip(bool state)
+    {
+        string finalText = "";
+        string path = "tooltips";
+        TextAsset csvFile = Resources.Load<TextAsset>(path) as TextAsset;
+        bool isLoaded = Localization.LoadCSV(csvFile);
+
+        // targetPos를 해당 gameobject의 local에서 계산 후 world position으로 변환
+        BoxCollider2D targetBox = GetComponent<BoxCollider2D>();
+        Transform targetTrans = GetComponent<Transform>();
+        Vector3 pos = new Vector3();
+        pos.x = targetBox.size.x / 2 + targetBox.offset.x;
+        pos.y = -targetBox.size.y / 2 + targetBox.offset.y;
+        pos = transform.TransformPoint(pos);
+
+        //// For debug //
+        //
+        ///* Get gameobject tree path */
+        //GameObject curObj = this.gameObject;
+        //var sb = new System.Text.StringBuilder();
+        //int i = 0;
+        //string tree = "";
+        //string curName = curObj.name;
+        //sb.Append(curName);
+        //while (curName != "UI Root/" || i != 10)
+        //{
+        //    i++;
+        //    if (curObj.transform.parent != null)
+        //    {
+        //        curObj = curObj.transform.parent.gameObject;
+        //        curName = curObj.name;
+        //        curName = string.Format("{0}/", curName);
+        //        sb.Insert(0, curName);
+        //    }
+        //    else
+        //    {
+        //
+        //        break;
+        //    }
+        //}
+        //tree = sb.ToString();
+        //
+        ///* Display values */
+        //Debug.Log(string.Format("Target Position in its Local Coordinate({0})\nX: {1}, Y: {2}", tree, pos.x, pos.y));
+        //Debug.Log(string.Format("Target Position in its World Coordinate after transform.TransformPosition({0})\nX: {1}, Y: {2}", tree, pos.x, pos.y));
+        //
+        //// End debug //
+
+        if (isLoaded)
+        {
+            //Debug.Log("CSV Loaded!");
+
+            finalText = Localization.Get(key);
+        }
+
+        if (!state)
+        {
+            finalText = "";
+        }
+
+        if (finalText != "")
+        {
+            // text와 position 같이 parameter로 전달
+            TooltipManager.ShowTooltip(finalText, pos);
+            //UITooltip.Show(finalText);
+        }
+        else
+        {
+            //Debug.Log("Error loading tooltip text");
+        }
+    }
+}
---
+++ tooltip/@BoxCollider2D/TooltipManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TooltipManager : UITooltip
+{
+    static protected TooltipManager myInstance;
+    void Awake() { myInstance = this; }
+    void OnDestroy() { myInstance = null; }
+    protected Vector3 targetPos;
+
+    protected void Set(string tooltipText, Vector3 pos)
+    {
+        targetPos = pos;
+        myInstance.SetText(tooltipText);
+    }
+
+    protected override void SetText(string tooltipText)
+    {
+        
+        base.SetText(tooltipText);
+
+        // Background 사이즈 조절
+        Vector4 border = background.border;
+        mSize.x += border.x + border.z - border.x * 2f;
+        mSize.y += border.y + border.w;
+        background.width = Mathf.RoundToInt(mSize.x);
+        background.height = Mathf.RoundToInt(mSize.y);
+
+        // target의 world position을 그대로 대입
+        mTrans.position = targetPos;
+        mPos = mTrans.localPosition;
+        mPos.x = Mathf.Round(mPos.x);
+        mPos.y = Mathf.Round(mPos.y);
+
+        // Local position에서 screen을 벗어나지 못하게 조정
+        if (mPos.x + mSize.x > Screen.width) mPos.x = Screen.width - mSize.x;
+        if (mPos.y - mSize.y < -Screen.height) mPos.y = -Screen.height + mSize.y;
+        //Debug.Log(string.Format("Tooltip position in its local coordintate(UI Root/Camera/Tooltip)\nX: {0}, Y: {1}", mPos.x, mPos.y));
+
+        mTrans.localPosition = mPos;
+
+        // Force-update all anchors below the tooltip
+        if (tooltipRoot != null) tooltipRoot.BroadcastMessage("UpdateAnchors");
+        else text.BroadcastMessage("UpdateAnchors");
+
+    }
+
+    static public void ShowTooltip(string text, Vector3 pos) { if (mInstance != null) myInstance.Set(text, pos); }
+}
